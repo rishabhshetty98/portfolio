38042f2895f96220142224410923f3bc36ae41b0
Rishabh Shetty <rishabhshettyvm@gmail.com>
2025-01-28 17:07:48 +0530

    Improving homepage (css pending)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,53 @@
 import React from "react";
+import Card from "../components/Card";
+import Button from "../components/Button";
+import InlineSkill from "../components/InlineSkill";
 
 const Home = () => {
+  const intro =
+    "I am Rishabh Shetty, a software developer with a penchant for AI.";
+  const skillset1 = ["Python", "FastAPI", "Django", "Flask"];
+  const skillset5 = ["Docker", "Kubernetes"];
+  const skillset2 = ["Javascript/Typescript", "ReactJS"];
+  const skillset3 = ["HTML", "CSS"];
+  const skillset4 = ["C++"];
+
   return (
-    <div>
-      <div className="card">
-        <h2>About Me</h2>
-      </div>
-      <div>
-        <h2>Skills</h2>
-      </div>
-      <div>
-        <h2>Experience</h2>
-      </div>
-      <div>
-        <h2>Download Resume</h2>
-      </div>
-    </div>
+    <>
+      <Card title="About Me">{intro}</Card>
+      <Card title="Skills">
+        <>
+          <p>Tech Stack</p>
+          <InlineSkill id="Backend" skills={skillset1} />
+          <InlineSkill id="Scipting" skills={skillset2} />
+          <InlineSkill id="Web" skills={skillset3} />
+          <InlineSkill id="cSkill" skills={skillset4} />
+          <InlineSkill id="containerSkill" skills={skillset5} />
+        </>
+      </Card>
+
+      <Card title="Experience">{intro}</Card>
+      <Card title="Dowload Resume">
+        <>
+          <p>To know more about me, feel free to take a look at my resume.</p>
+          <Button children="Download" onClick={() => console.log("Clicked")} />
+        </>
+      </Card>
+    </>
+
+    // <div>
+    //   <div className="card">
+    //     <div className="card-header">
+    //       <h2 className="card-title">About Me</h2>
+    //     </div>
+    //     <div className="card-body">
+    //       <p className="card-text">
+    //         {"I am a passionate Software Developer with a curiousity of AI."}
+    //       </p>
+    //     </div>
+    //   </div>
+
+    // </div>
   );
 };
 
